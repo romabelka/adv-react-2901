87f73406fadd2b668d14caf394e3f8c68268f648
Igor Zvyagin <spirit-drive@yandex.ru>
2019-02-03 18:06:11 +0300

    HT1.1

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {Route, NavLink} from 'react-router-dom';
-import {connect} from 'react-redux';
+import {Route, NavLink, Switch} from 'react-router-dom';
 import AuthPage from './components/routes/auth';
 import AdminPage from './components/routes/admin';
+import ProtectedRoute from './components/routes/ProtectedRoute';
 
 class App extends Component {
   render() {
@@ -19,8 +19,10 @@ class App extends Component {
           </ul>
         </nav>
         <section>
-          <Route path="/auth" component={AuthPage}/>
-          <Route path="/admin" component={AdminPage}/>
+          <Switch>
+            <Route path="/auth" component={AuthPage}/>
+            <ProtectedRoute path="/admin" component={AdminPage}/>
+          </Switch>
         </section>
       </div>
     )
---
+++ admin/src/components/routes/ProtectedRoute.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import { Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, user, ...rest }) => {
+  if (!user) return <Redirect to="/auth" {...rest} />;
+  return <Component />;
+};
+
+export default connect(state => ({ user: state.auth.user}))(ProtectedRoute);
