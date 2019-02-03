4468146e51bb9326ddfacbf10ac0da7315f8ff61
Igor Zvyagin <spirit-drive@yandex.ru>
2019-02-03 21:56:04 +0300

    feat: using loader

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Route, NavLink, Switch} from 'react-router-dom';
-import {connect} from 'react-redux'
 import withInit from './hoc/withInit';
 import AuthPage from './components/routes/auth';
 import Header from './components/Header';
@@ -8,34 +7,30 @@ import AdminPage from './components/routes/admin';
 import ListUsers from './components/routes/ListUsers';
 import ProtectedRoute from './components/routes/ProtectedRoute';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/auth" activeStyle={{color: 'red'}}>auth</NavLink>
-            </li>
-            <li>
-              <NavLink to="/admin" activeStyle={{color: 'red'}}>admin</NavLink>
-            </li>
-            <li>
-              <NavLink to="/list-users" activeStyle={{color: 'red'}}>list-users</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <section>
-          <Switch>
-            <Route path="/auth" component={AuthPage}/>
-            <ProtectedRoute path="/admin" component={AdminPage}/>
-            <ProtectedRoute path="/list-users" component={ListUsers}/>
-          </Switch>
-        </section>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Header/>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/auth" activeStyle={{color: 'red'}}>auth</NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin" activeStyle={{color: 'red'}}>admin</NavLink>
+        </li>
+        <li>
+          <NavLink to="/list-users" activeStyle={{color: 'red'}}>list-users</NavLink>
+        </li>
+      </ul>
+    </nav>
+    <section>
+      <Switch>
+        <Route path="/auth" component={AuthPage}/>
+        <ProtectedRoute path="/admin" component={AdminPage}/>
+        <ProtectedRoute path="/list-users" component={ListUsers}/>
+      </Switch>
+    </section>
+  </div>
+);
 
 export default withInit(App)
