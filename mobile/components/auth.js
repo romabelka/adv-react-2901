c54e8b0dcab9ea04b25b08d89dbc151bace9dbd5
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-02-22 19:37:03 +0200

    add simple form

---
+++ mobile/components/auth.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import {View, Text, TextInput, Button, Platform} from 'react-native'
+
+class Auth extends Component {
+    static propTypes = {}
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleEmailChange = (email) => this.setState({email})
+    handlePasswordChange = (password) => this.setState({password})
+    handleSignIn = () => console.log('---', this.state)
+
+    render() {
+        return (
+            <View>
+                <Text style={{fontSize: 30}}>
+                    Email
+                </Text>
+                <TextInput
+                    style={styles.input}
+                    value={this.state.email} onChangeText={this.handleEmailChange} keyboardType="email-address"/>
+                <Text>
+                    Password
+                </Text>
+                <TextInput
+                    style={styles.input}
+                    value={this.state.password} onChangeText={this.handlePasswordChange} secureTextEntry/>
+                <Button title="SignIn" onPress={this.handleSignIn} />
+            </View>
+        )
+    }
+}
+
+const styles = {
+    input: {
+        ...Platform.select({
+            ios: {
+                borderBottomWidth: 1,
+
+            },
+            android: {
+            }
+        })
+    }
+}
+
+export default Auth
