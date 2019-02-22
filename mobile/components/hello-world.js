c59bd6aa4d8ad0f46dc6ab370bceadbf9ba99c0c
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-02-22 19:08:53 +0200

    add HelloWorld

---
+++ mobile/components/hello-world.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Hello
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default HelloWorld
