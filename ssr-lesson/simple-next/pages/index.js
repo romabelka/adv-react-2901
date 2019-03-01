7d9f45f776e80bccd19cc968825931ebebb81b08
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-03-01 20:01:18 +0200

    add HT8

---
+++ ssr-lesson/simple-next/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from "../components/layout";
+/*
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+import Layout from "../components/layout";
+*/
+
+const Home = () => (
+    <Layout title="main">
+        <h1>Main Page</h1>
+    </Layout>
+)
+
+export default Home
